d5568ab42ec5d79d66feb0bf7b8a23f080174d54
Luciano <lu.camano@duocuc.cl>
2024-10-15 01:50:37 -0300

    Vistas principales creadas

--- pagina/urls.py
+++ pagina/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('login/', views.login, name="login"),
+    path('marcas/', views.marcas, name="marcas"),
+    path('categorias/', views.categorias, name="categorias"),
+    path('producto/', views.producto, name="producto"),
+    path('productos/', views.productos, name="productos"),
+    path('administracion/', views.administracion, name="administracion"),
 ]
--- pagina/views.py
+++ pagina/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "index.html")
+
+def login(request):
+    return render(request, "login.html")
+
+def marcas(request):
+    return render(request, "marcas.html")
+
+def categorias(request):
+    return render(request, "categorias.html")
+
+def producto(request):
+    return render(request, "producto.html")
+
+def productos(request):
+    return render(request, "productos.html")
+
+def administracion(request):
+    return render(request, "administracion.html")
