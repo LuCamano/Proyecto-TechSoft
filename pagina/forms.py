313949a989bba095709e206e80014914490e7854
Luciano <lu.camano@duocuc.cl>
2024-10-24 15:39:32 -0300

    Formulario de agregar carácteristica

--- pagina/forms.py
+++ pagina/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Producto
+from .models import Producto, ProductoCaracteristica
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Column, Row, Field, Div
 from crispy_bootstrap5.bootstrap5 import FloatingField, Switch, Accordion
@@ -30,4 +30,18 @@ class LoginForm(AuthenticationForm):
             FloatingField('username'),
             FloatingField('password'),
             Submit('submit', 'Iniciar Sesión', css_class="btn-success")
+        )
+
+class ProductoCaracteristicaForm(forms.ModelForm):
+    class Meta:
+        model = ProductoCaracteristica
+        fields = ['producto', 'caracteristica', 'descripcion_caract']
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.disable_csrf = True
+        self.helper.layout = Layout(
+            FloatingField("caracteristica"),
+            FloatingField("descripcion_caract")
         )
--- pagina/views.py
+++ pagina/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.views.generic import CreateView
 from django.contrib.auth.views import LoginView
-from .forms import ProductoForm, LoginForm
+from .forms import ProductoForm, LoginForm, ProductoCaracteristicaForm
 from .models import Producto, Marca, Categoria, Caracteristica
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -36,6 +36,7 @@ def producto(request):
 def productos(request):
     return render(request, "productos.html")
 
+@login_required
 def adminProductos(request):
     productos = Producto.objects.all()
     context = {
@@ -43,24 +44,29 @@ def adminProductos(request):
     }
     return render(request, "administracion_productos.html", context)
 
+@login_required
 def adminCaracteristicas(request):
     caracts = Caracteristica.objects.all()
     context = { 'caracteristicas': caracts }
     return render(request, "administracion_caracteristicas.html", context)
 
+@login_required
 def adminMarcas(request):
     marcas = Marca.objects.all()
     context = { 'marcas': marcas }
     return render(request, "administracion_marcas.html", context)
 
+@login_required
 def adminCategorias(request):
     categs = Categoria.objects.all()
     context = { 'categorias': categs }
     return render(request, "administracion_categorias.html", context)
 
+@login_required
 def administracion(request):
     return redirect('/administracion/productos')
 
+@login_required
 def caractProducto(request, id):
     producto = Producto.objects.get(id=id)
     context = {
@@ -80,3 +86,19 @@ class AgregarProducto(LoginRequiredMixin, CreateView):
         context['titulo'] = 'Agregar Producto'
         context['urlAnterior'] = '/administracion/'
         return context
+    
+@login_required
+def asignarCaracteristicas(request:HttpRequest, idP):
+    if request.method == "POST":
+        form = ProductoCaracteristicaForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('administracion')
+    product = Producto.objects.get(id=idP)
+    form = ProductoCaracteristicaForm(producto=product)
+    context = { 
+        'form': form,
+        'titulo': 'Agregar una característica',
+        'urlAnterior': '/administracion/'
+        }
+    return render(request, "formulario.html", context)
